ec9dd64e4d0983da55b2dd3cc732310fee5627d8
Aisha <aisha.nadaf@bitclass.live>
2022-03-19 14:12:28 +0530

    Test case added for removing movie from the list

--- src/main/java/tmdb/CreateList.java
+++ src/main/java/tmdb/CreateList.java
@@ -24,4 +24,45 @@ public class CreateList {
                 .log()
                 .all().extract().response();
     }
+    @Test(priority = 1)
+    public void AddMovie(){
+        String payload = "{\n" +
+                "  \"media_id\": 14911\n" +
+                "}";
+        given()
+                .baseUri("https://api.themoviedb.org")
+                .body(payload)
+                .contentType("application/json")
+                .when()
+                .queryParam("session_id", "9078f551bfb19d83403ca9880bf6a5a16444a5f3")
+                .queryParam("api_key","8dde48da411a7993668563ab06b893a1")
+                .post("3/list/8195512/add_item")
+                .then()
+                .statusCode(201)
+                .log()
+                .all()
+                .extract()
+                .response();
+    }
+    @Test(priority = 2)
+    public void deleteMovie(){
+        String payload = "{\n" +
+                "  \"media_id\": 20453\n" +
+                "}";
+        given()
+                .baseUri("https://api.themoviedb.org")
+                .body(payload)
+                .contentType("application/json")
+                .when()
+                .queryParam("session_id", "9078f551bfb19d83403ca9880bf6a5a16444a5f3")
+                .queryParam("api_key","8dde48da411a7993668563ab06b893a1")
+                .post("3/list/8195512/remove_item")
+                .then()
+                .statusCode(200)
+                .log()
+                .all()
+                .extract()
+                .response();
+
+    }
 }
